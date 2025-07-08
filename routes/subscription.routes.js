17e0feeefe700e539050c1d1ccb4035a63c618ea
Paul Gomis <gomispapaul6@gmail.com>
2025-07-08 16:34:00 +0000

    Implementer la souscription

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,18 +1,20 @@
 import { Router } from 'express';
+import authorize from '../middlewares/auth.middleware.js';
+import { createSubscription, getSubscription, getSubscriptions, getUserSubscriptions } from '../controllers/subscription.controller.js';
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.get('/', (req, res) => res.send({ title: 'GET all subscriptions' }))
+subscriptionRouter.get('/', authorize, getSubscriptions)
 
-subscriptionRouter.get('/:id', (req, res) => res.send({ title: 'GET subscription details' }))
+subscriptionRouter.get('/:id', authorize, getSubscription)
 
-subscriptionRouter.post('/', (req, res) => res.send({ title: 'CREATE new subscription' }))
+subscriptionRouter.post('/', authorize, createSubscription)
 
 subscriptionRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE a subscription' }))
 
 subscriptionRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE a subscription' }))
 
-subscriptionRouter.get('/user/:id', (req, res) => res.send({ title: 'GET all user subscriptions' }))
+subscriptionRouter.get('/user/:id', authorize, getUserSubscriptions)
 
 subscriptionRouter.put('/:id/cancel', (req, res) => res.send({ title: 'CANCEL subscription' }))
 
